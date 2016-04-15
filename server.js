3a1887b22ba291eae1d2c6e9cfe8d4446ebc87e1
Jimmy Lin <usmcjimmy@gmail.com>
2016-04-15 14:44:05 -0400

    added ,

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if(process.env.ENVIORNMENT === 'production') {
 } else {
   var connectionString = {
     host: process.env.DB_HOST,
-    port: 5432
+    port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
